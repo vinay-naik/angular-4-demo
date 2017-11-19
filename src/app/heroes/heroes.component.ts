129ec9f64aa0b56fb7e6e3c155daedb60cc32e5b
Vinay Naik <vinay@browntape.com>
2017-11-19 20:13:12 +0530

    Learnt how to add routes and about http client

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,23 +8,30 @@ import { HeroService } from '../hero.service';
 	styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-
 	heroes: Hero[];
 
-	selectedHero: Hero;
-
-	onSelect(hero: Hero): void {
-		this.selectedHero = hero;
-	}
-
-	getHeroes(): void {
-		this.heroService.getHeroes()
-			.subscribe(heroes => this.heroes = heroes);
-	}
 	constructor(private heroService: HeroService) { }
 
 	ngOnInit() {
 		this.getHeroes();
 	}
 
+	getHeroes(): void {
+		this.heroService.getHeroes()
+			.subscribe(heroes => this.heroes = heroes);
+	}
+
+	add(name: string): void {
+		name = name.trim();
+		if (!name) { return; }
+		this.heroService.addHero({ name } as Hero)
+			.subscribe(hero => {
+				this.heroes.push(hero);
+			});
+	}
+
+	delete(hero: Hero): void {
+		this.heroes = this.heroes.filter(h => h !== hero);
+		this.heroService.deleteHero(hero).subscribe();
+	}
 }
